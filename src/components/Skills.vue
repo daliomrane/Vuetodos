7dd402d1ae8d40e484a26bfa87e0e52ba4c41077
dalibouomrane <dalibouomrane@gmail.com>
2019-11-06 23:02:47 +0100

    6-11-2019 modify Skills component

---
+++ src/components/Skills.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    {{name}}
+    {{btnState ? 'button disabled' :'button enabled' }}
+    <button
+      v-on:click="changeName"
+      v-bind:disabled="btnState"
+    >Change Name</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Skills",
+  data() {
+    return {
+      name: "TestName",
+      btnState: false
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
