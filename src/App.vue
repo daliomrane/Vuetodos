514e1cbbf96bc014df3c56c466147ad380ccfb2a
dalibouomrane <dalibouomrane@gmail.com>
2019-11-16 12:33:01 +0100

    Styling Skills.vue Template

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,37 @@
 <template>
-  <div id="app">
-    <Skills />
-  </div>
+	<div id="app">
+		<Skills />
+	</div>
 </template>
 
 <script>
 import Skills from "./components/Skills.vue";
 
 export default {
-  name: "app",
-  components: {
-    Skills
-  }
+	name: "app",
+	components: {
+		Skills
+	}
 };
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,700");
+
+body {
+	background-color: #eeeeee;
+	font-family: "Montserrat", sans-serif;
+	display: grid;
+	grid-template-rows: auto;
+	justify-items: center;
+	align-items: center;
+}
+body,
+html {
+	margin: 0;
+	height: 100%;
+}
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	width: 50%;
 }
 </style>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,37 +1,79 @@
 <template>
-  <div class="hello">
-    <div class="holder">
-      <ul>
-        <li v-for="(data,index) in skills" :key="index">{{index}}. {{data.skill}}</li>
-      </ul>
+	<div class="hello">
+		<div class="holder">
+			<form @submit.prevent="addSkill">
+				<input
+					type="text"
+					placeholder="Enter a skill you Have"
+					v-model="skill"
+				/>
+			</form>
 
-      <div v-bind:class="{alert : showAlert, 'anotherclass':!showClass}"></div>
-    </div>
-  </div>
+			<ul>
+				<li v-for="(data, index) in skills" :key="index">
+					{{ index }}. {{ data.skill }}
+				</li>
+			</ul>
+			<p>These are the skills that i possess.</p>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: "Skills",
-  data() {
-    return {
-      skills: [{ skill: "Vue.js" }, { skill: "FrontEnd-Dev" }],
-      showAlert: false,
-      showClass: true
-    };
-  }
+	name: "Skills",
+	data() {
+		return {
+			skill: "",
+			skills: [{ skill: "Vue.js" }, { skill: "FrontEnd-Dev" }]
+		};
+	},
+	methods: {
+		addSkill() {
+			this.skills.push({ skill: this.skill });
+			this.skill = "";
+		}
+	}
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.alert {
-  background-color: yellow;
-  width: 100%;
-  height: 30px;
+.holder {
+	background: #fff;
 }
 
-.anotherclass {
-  background-color: red;
+ul {
+	margin: 0;
+	padding: 0;
+	list-style-type: none;
+}
+
+ul li {
+	padding: 20px;
+	font-size: 1.3em;
+	background-color: #e0edf4;
+	border-left: 5px solid #3eb3f6;
+	margin-bottom: 2px;
+	color: #3e5252;
+}
+
+p {
+	text-align: center;
+	padding: 30px 0;
+	color: gray;
+}
+
+.container {
+	box-shadow: 0px 0px 40px lightgray;
+}
+
+input {
+	width: calc(100% - 40px);
+	border: 0;
+	padding: 20px;
+	font-size: 1.3em;
+	background-color: #323333;
+	color: #687f7f;
 }
 </style>
